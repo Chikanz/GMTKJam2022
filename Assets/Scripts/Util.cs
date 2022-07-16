a344a52f1ed8a3be5271be61f81a68b7268e62af
DESKTOP-9411KA9\dook <zac.lucarelli@outlook.com>
2022-07-16 23:11:08 +1000

    Interest bar UI

---
+++ Assets/Scripts/Util.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Util{
+    public Vector3 RandomNavmeshLocation(float radius) {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1)) {
+            finalPosition = hit.position;            
+        }
+        return finalPosition;
+    }
+}
